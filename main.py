72edd135342daf11991a71641c1cd5faf00ed9d5
Dan <danelevation@gmail.com>
2025-12-05 09:45:35 +0000

    Using async httpx client to get ids. Working for 10 ids. Going to time for all results

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
-def main():
-    print("Hello from explore-fenz!")
+import asyncio
+import httpx
+import polars as pl
 
+async def fetch_all(endpoint, ids):
+    async with httpx.AsyncClient() as client:
+        tasks = []
+        for id in ids:
+            tasks.append(client.get(f"{endpoint}?cmpId={id}"))
+    
+        responses = await asyncio.gather(*tasks, return_exceptions=True)
+
+        valid_data = []
+        for rep in responses:
+            if isinstance(rep, httpx.Response) and rep.status_code == 200:
+                valid_data.append(rep.json())
+        return valid_data
+
+def main():
+    ids = range(1, 10)
+
+    data = asyncio.run(fetch_all("https://api.fencing.org.nz/public/results",
+    ids))
+    print(f"Fetched {len(data)} records")
+
+    df = pl.DataFrame(data)
+
+    print(df.head)
 
 if __name__ == "__main__":
     main()
