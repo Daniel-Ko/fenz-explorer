58079e15b11d2a6b6b3f6ec0fb462c06cff2fa46
Dan <danelevation@gmail.com>
2025-12-08 16:00:26 +0000

    Manually closing client.
    Every time I make a batch request, there are a few that are not working. I hope to give some respite to the client by managing the disconnection myself, as I notice the last 2 ids are always not going through. I fear this is a temp solution. I will add the user agent string and try a bigger batch for my next test...about 50 ids. This will show if there are more failures scattered through a bigger request

--- main.py
+++ main.py
@@ -5,41 +5,49 @@ import polars as pl
 
 from loguru import logger
 
-SEMAPHORE_LIMIT = 1
+SEMAPHORE_LIMIT = 50
 
-class ApiTempError(Exception):
+class ApiResponseError(Exception):
     pass
 
 @retry(
     stop=stop_after_attempt(5)
     ,wait=wait_random_exponential(min=1, max=10)
-    ,retry=retry_if_exception_type(httpx.RequestError)
+    ,retry=retry_if_exception_type(ApiResponseError)
 )
 async def fetch_id(client, id_num, endpoint, semaphore):
     async with semaphore:
         try:
             response = await client.get(endpoint, timeout=10.0)
 
-            # if response.status_code in (429, 500, 502, 503, 504, 508):
-            #    raise ApiTempError(f"{endpoint}")
+            if response.status_code in (429, 500, 502, 503, 504, 508):
+                logger.debug(f"Error {response.status_code} with id:{id_num}. Retrying")
+                raise ApiResponseError()
+                #raise httpx.HTTPStatusError(
+                #    request=f"{endpoint}",
+                #    response=response.status_code,
+                #    message="Trouble fetching this id. Retrying."
+                #)
             return response
         except httpx.RequestError as e:
             logger.exception(f"Network Error - {id_num}: {endpoint}, {e}")
-            raise e
+            raise ApiResponseError()
         except Exception as e:
             logger.exception(f"Unexpected error - {id_num}: {endpoint}, {e}")
-            return e
+            raise e
 
-async def fetch_all_with_ids(endpoint, param, ids):
+async def fetch_all_with_ids(base_url, param, id_range):
     semaphore = asyncio.Semaphore(SEMAPHORE_LIMIT)
+    
+    client = httpx.AsyncClient()
 
-    async with httpx.AsyncClient() as client:
+    try:
         tasks = []
-        for id in ids:
+        for id in id_range:
             tasks.append(fetch_id(
                 client=client, 
                 id_num=id, 
-                endpoint=f"{endpoint}?{param}={id}", 
+                endpoint=f"{base_url}?{param}={id}", 
                 semaphore=semaphore
             ))
     
@@ -51,18 +59,25 @@ async def fetch_all_with_ids(endpoint, param, ids):
                 if rep.status_code == 200:
                     valid_data.append(rep.json())
                 else:
-                    logger.debug(f"Error: Status {rep.status_code} for id {i}")
+                    logger.debug(f"Error: Status {rep}")
             else:
-                logger.exception(f"Network or Timeout error: {rep.status_code} for id {i}")
+                logger.exception(f"Network or Timeout error: {rep}")
         return valid_data
+    finally:
+        await client.aclose()
 
 def main():
 #    ids = range(1, 1422)
-    ids = range(1, 100)
+    ids = range(1, 10)
     
     endpoint = "https://api.fencing.org.nz/public/results"
     param = "cmpId"
-    data = asyncio.run(fetch_all_with_ids("https://api.fencing.org.nz/public/results",param,ids))
+    data = asyncio.run(
+        fetch_all_with_ids(
+            base_url="https://api.fencing.org.nz/public/results",
+            param=param,
+            id_range=ids
+    ))
     logger.info(f"Fetched {len(data)} records")
 
     df = pl.DataFrame(data)
