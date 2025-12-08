916cb25250eacfa0526f13b80e722459e76ef8da
Dan <danelevation@gmail.com>
2025-12-08 17:08:25 +0000

    I have determined that at least some of these ids are actual 500 errors.
    500 errors for certain ids which I will log. We may be able to use the async script after all. Will just have to ignore errors.
    
    I tested by sending api requests through an API inspection client and getting 500. I retested on the website itself and indeed, 500 response.

--- test_sequential.py
+++ test_sequential.py
@@ -2,25 +2,41 @@ import httpx
 import polars as pl
 from loguru import logger
 
-def main():
-#    ids = range(1, 1422)
-    id_range = range(1, 25)
-    
-    data = []
+data = [0]*25
 
-    base_url = "https://api.fencing.org.nz/public/results"
-    param = "cmpId"
+def fetch_all(base_url, id_range) -> list[int]:
+    bad_records = []
+
     for id_num in id_range:
         rep = httpx.get(f"{base_url}?cmpId={id_num}")
         if rep.status_code == 200:
-           data.append(rep.json())
+           data[id_num] = rep.json()
         else:
-            logger.warning(f"ID {id_num} failed with status {rep.status_code}. {rep.text[:100]}")
+            bad_records.append(id_num)
+            logger.warning(f"ID {id_num} failed with status {rep.status_code}.")
             pass
-    logger.info(f"Fetched {len(data)} records")
-    
+    return bad_records
 
-    df = pl.DataFrame(data).with_row_index(offset=1)
+def main():
+#    ids = range(1, 1422)
+    id_range = range(1, 25)
+
+    base_url = "https://api.fencing.org.nz/public/results"
+
+    bad_records = fetch_all(base_url, id_range)
+    logger.debug(f"BAD RECORDS: {bad_records}")
+
+    # retry bad records a couple of times, hoping the list sorts itself out slowly.
+    i = 5
+    while i > 0 and bad_records:
+        logger.debug(f"{len(bad_records)} bad records found: {bad_records}. Retrying.")
+        fetch_all(base_url, iter(bad_records))
+        i -= 1
+
+    logger.info(f"Fetched {len(data)-len(bad_records)} records")
+    
+    pl.Config.set_tbl_rows(100)
+    df = pl.DataFrame(data, strict=False).with_row_index(offset=1)
 
     print(df)
 
