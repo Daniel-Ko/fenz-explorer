36f5adbf0bee7b01d8940c03875204925e5452ff
Dan <danelevation@gmail.com>
2025-12-18 06:17:15 +0000

    Finished all silver tables in the data model I envisioned from RESULTS.
    There will be 'athletes' later but this will be all competition results.

---
+++ src/sparksql/events_silver.sql
@@ -0,0 +1,30 @@
+CREATE OR REPLACE TABLE events_silver
+USING DELTA
+PARTITIONED BY (short_desc, category)
+AS
+SELECT
+    -- need all 3 to make a unique id
+   CONCAT(cmp_id, '_', event_id, '_', short_desc) AS event_key  
+  ,CAST(event_id AS INT)             AS event_id
+  ,CAST(cmp_id AS INT)               AS cmp_id
+  ,CAST(event AS INT)                AS event_num 
+  ,category                          AS category 
+  ,short_desc                        AS short_desc 
+  ,long_desc                         AS long_desc
+FROM (
+  SELECT
+    cmp.cmp_id,
+    event_struct.*,
+    ROW_NUMBER() OVER (
+      PARTITION BY CONCAT(cmp.cmp_id, '_', event_struct.event_id, '_', event_struct.short_desc)
+      ORDER BY event_struct.event_id
+    ) AS rn
+  FROM results_bronze
+  LATERAL VIEW explode(availEvents) AS event_struct
+  )
+  WHERE rn = 1
+;
+OPTIMIZE EVENTS_SILVER ZORDER BY (event_id);
+
+ALTER TABLE EVENTS_SILVER ALTER COLUMN event_key SET NOT NULL;
+ALTER TABLE EVENTS_SILVER ADD PRIMARY KEY (event_key);
---
+++ src/sparksql/results_silver.sql
@@ -0,0 +1,86 @@
+CREATE OR REPLACE TABLE RESULTS_SILVER 
+USING DELTA
+AS
+WITH json_friendly_events AS (
+  SELECT 
+    cmp.cmp_id,
+    regexp_replace(
+      regexp_replace(
+        regexp_replace(events, 
+          '\'detailedResults\': ?True', '\'detailedResults\': true'),
+        '\'detailedResults\': ?False', '\'detailedResults\': false'),
+      'None', 'null'
+    ) AS events_cleaned
+  FROM results_bronze
+),
+exploded_results AS (
+  SELECT
+    cmp_id,
+    event_number,
+    event_details
+  FROM (
+    SELECT
+      cmp_id,
+      explode(
+        from_json(
+          events_cleaned,
+          'MAP<STRING, STRUCT<
+            detailedResults:BOOLEAN,
+            drawPouleRound: STRUCT<
+              poules: MAP<STRING, MAP<STRING, STRUCT<
+                matches: MAP<STRING, STRING>,
+                name: STRING
+              >>>
+            >,
+            drawPouleResults: MAP<STRING, STRUCT<
+              name: STRING,
+              club: STRING,
+              poule: STRING,
+              poule_pos: STRING,
+              initial: STRING,
+              eligable: STRING
+            >>,
+            drawTableaux: MAP<STRING, STRUCT<
+              tableau_data: ARRAY<STRUCT<
+                name: STRING,
+                points_for: STRING,
+                points_against: STRING,
+                code: STRING,
+                pos: INT
+              >>,
+              tab_size: INT
+            >>,
+            drawTournResults:ARRAY<STRUCT<
+              place:STRING,
+              uid:STRING,
+              name:STRING,
+              region:STRING,
+              club:STRING,
+              country:STRING
+            >>
+          >>'
+        )
+      ) AS (event_number, event_details)
+    FROM json_friendly_events
+  )
+)
+
+SELECT
+  e.event_key
+  ,e.event_id
+  ,cast(r.cmp_id as int)
+  ,cast(r.event_number as int)
+  ,r.event_details.detailedResults AS detailedResults
+  ,r.event_details.drawPouleRound AS drawPouleRound
+  ,r.event_details.drawPouleResults AS drawPouleResults
+  ,r.event_details.drawTableaux AS drawTableaux
+  ,r.event_details.drawTournResults AS drawTournResults
+FROM exploded_results r
+INNER JOIN events_silver e
+  ON r.event_number = e.event_num
+  AND r.cmp_id = e.cmp_id
+;
+OPTIMIZE RESULTS_SILVER ZORDER BY (event_key);
+
+ALTER TABLE RESULTS_SILVER ALTER COLUMN event_key SET NOT NULL;
+ALTER TABLE RESULTS_SILVER ADD PRIMARY KEY (event_key);
